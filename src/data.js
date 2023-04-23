5ed7db8700d973c6cbc2e13912749d35d89e9b72
Arnab Mitra <mitraarnab.2013@gmail.com>
2023-04-23 15:45:35 +0530

    Deploy

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const projects = [
+    {
+      title: "Tic Tac Toe App/Game",
+      subtitle: "React",
+      description:
+        "Built this simple Tic Tac Toe game using React only. Used JavaScript to write a simple logic and a very basic and intuitive UI.",
+      image: "./project-1.gif",
+      link: "https://github.com/miarnab/TicTacToeApp-ReactJS",
+    },
+    {
+      title: "TV Series App",
+      subtitle: "React",
+      description:
+        "Built this simple TV Series App using React only. Used component or class based approach to fetch the searched keyword from API and return the list of TV Series to us.",
+      image: "./project-2.gif",
+      link: "https://github.com/miarnab/TVSeriesApp-ReactJS",
+    },
+    {
+      title: "URL Shortener App",
+      subtitle: "React",
+      description:
+        "Built thsi simple URL Shortener App using React only. Used the Bitly API to get the shortened form of inputed URL.",
+      image: "./project-3.gif",
+      link: "https://github.com/miarnab/URLShortenerApp-ReactJS",
+    },
+    {
+      title: "Movies Recommendation System ",
+      subtitle: "React and Flask/Node",
+      description:
+        "Built this Movies Recommendation System App/Website using React on Frontend and Flask/Node on Backend. Uses the Neo4j recommendation system to recommend movies.",
+      image: "./project-4.gif",
+      link: "https://github.com/miarnab/IMDB-Clone-And-Recommendation-Neo4j-Node-Flask-React",
+    },
+  ];
+
+  export const skills=[
+    "Java, Python, Spring, React, HTML, CSS, JavaScript, React Native, React Hooks, Redux, React Saga",
+  ];
+
+  export const testimonials = [
+    {
+      quote: "Have shown excellent skills and time management",
+      image: "",
+      name: "Kinjal Mitra",
+      company: "Tata Consultancy Services",
+    },
+    {
+      quote: "Highly skilled in the given skills and excellent team player",
+      image: "",
+      name: "Sayan Chowdhury",
+      company: "Cognizant",
+    },
+    {
+      quote: "Great analytical skills and critical problem solver",
+      image: "",
+      name: "Agniva Saha",
+      company: "Wipro",
+    },
+    {
+      quote: "Have shown excellent designing skills and highly skilled in React and React Native",
+      image: "",
+      name: "Sanjib Pal Chowdhury",
+      company: "Tata Consultancy Services",
+    },
+
+  ];
